a3292a75070f70ef8c321c7d90a40b5fd253b887
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-23 19:32:33 +0100

    feat: Trying to get options to show in selected.
    Trying to import categories to <select>

--- src/components/SelectionItems.vue
+++ src/components/SelectionItems.vue
@@ -1,15 +1,20 @@
 <script setup>
   import { useStore } from 'vuex';
-  import {computed} from "vue";
+  import {computed, ref} from "vue";
 
 
   const store = useStore
   const categories = computed(() => store.getters.getCategories)
+  const selected = ref("");
 </script>
 
 <template>
-  <div>
-
+  <div id="select-category">
+    <select v-model="selected">
+      <option v-for="category in categories" :key="category.id" :value="category.id">
+        {{ category.name }}
+      </option>
+    </select>
   </div>
 </template>
 <style scoped>
