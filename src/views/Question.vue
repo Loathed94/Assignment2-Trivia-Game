5e07d86b5094de3c186d052b7899a636fb4d0df9
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 23:41:07 +0100

    add checkboxes

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,6 +1,5 @@
 <!--Components that are linked to the router-->
 <script setup>
-
 </script>
 
 <template>
@@ -15,16 +14,44 @@
 
     <fieldset class="mb-5">
         <!--Checkbuttons with answers generate from the API-->
-        <label class="block">Question name</label><br>
-        <input type="checkbox" class="block" id="question.name"/>
-        <input type="checkbox" class="block" id="question.name"/>
-        <input type="checkbox" class="block" id="question.name"/>
-        <input type="checkbox" class="block" id="question.name"/>
+        <label class="block">Question name</label>
+        <div id="answerCheckbox">
+            <label for="firstOption">Answer number 1</label>
+            <input 
+            type="checkbox" 
+            id="firstOption"
+            v-model="checkedNames"/><br>
 
-    </fieldset>
+            <label for="secondOption">Answer number 2</label>
+            <input
+            type="checkbox" 
+            id="secondOption"
+            v-model="checkedNames"/><br>
 
-<!--Submit the answer and count the points, redirect to result page-->
-<button type="submit" class="bg-green-400 text-white p-2 rounded">Submit answer</button>
+            <label for="secondOption">Answer number 3</label>
+            <input 
+            type="checkbox" 
+            id="thirdOption"
+            v-model="checkedNames"/><br>
+            
+            <label for="secondOption">Answer number 4</label>
+            <input 
+            type="checkbox" 
+            id="fourthOption" 
+            v-model="checkedNames"/>
+            
+
+        </div>
+      </fieldset>  
+        <!-- <input type="checkbox" class="block" id="question.name"/>
+
+    
+
+Submit the answer and count the points, redirect to result page-->
+<div class="form-group">
+    <button class="btn btn-primary bg-green-400 text-white p-2 rounded">Submit answer</button>
+</div>
+<!-- <button type="submit" class="bg-green-400 text-white p-2 rounded">Submit answer</button> -->
 
 </form>
 </main>
