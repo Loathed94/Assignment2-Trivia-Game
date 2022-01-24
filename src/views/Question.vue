d2a54ef8b37e9c3994a6605aee3af21ba41f3fea
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 16:12:33 +0100

    start adding code for checkbox area at Question page Question.vue

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
+    <!--Question 1-->
 
-  </div>
+    <!--Create checkbox for answers of questions-->
+    <form @submit.prevent = "handleSubmit">
+      <div class = "">
+        <label for = "title">Question</label>
+        <input type="checkbox" id = "checkbox"/>
+      </div>  
+
+  <!--Submit the answer-->
+      <div class = "submitAnswer">
+      </div>
+      <div class = "submitAnswer">
+        <button class = "submitAnswerBtn" type = "submit">Submit answer</button>
+      </div>
+    </form>
 </template>
 
-<script setup>
-
+<!-- <script setup>
+//Show answer choices
+  export default {
+    data() {
+      Answers: [
+        {}
+        {}
+        {}
+        {} -->
+      ]
+    }
+  }
 </script>
 
 <style scoped>
