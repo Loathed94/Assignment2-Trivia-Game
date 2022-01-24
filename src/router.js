bfc00b2677ed4a2981ce4a3a3812a57459aac671
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 22:02:09 +0100

    add Question.vue to router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Question from './views/Question.vue';
 
-const routes = [];
+const routes = [
+    {
+        path: "/",
+        component: Question
+    }
+];
 
 export default createRouter({
     history: createWebHistory(),
