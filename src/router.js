aef35143ce907f4a2706cc2ffd55f4a1a5c777ce
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 15:32:22 +0100

    add Question.vue view to router.js

--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import {createRouter, createWebHistory} from "vue-router";
+import Start from './views/Start.vue'
+import Question from './views/Question.vue';
 import store from "./store";
 
 const routes = [
     {
         path: "/",
-        component: "StartScreen",
+        component: Start,
         beforeEnter: "nån authGuard-aktig-grej"
     },
+    {
+        path:"",
+        component: "QuestionScreen",
+    }
 ];
 
 
