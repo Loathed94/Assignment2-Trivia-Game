805b46717e5bab7262c7e890666780dc3842614b
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-25 00:25:51 +0100

    add fetch request at game.js

--- src/api/game.js
+++ src/api/game.js
@@ -1,1 +1,13 @@
 import {BASE_URL} from './';
+
+export async function apiAnswerRegister(answerCheckbox) {
+    try {
+        const response = await fetch(`${BASE_URL_Question_DB}/api.php?amount=${questionQuantity}&category=${questionCategory}&difficulty=${questionDifficulty}&type=${questionType}`);
+        const data = await response.json();
+        return [data.response_code, data.results];
+    }
+    catch(error) {
+        return [[], error.message];
+
+    }
+}
