4d517f90fd53d80f5758ec3e406255348fe689d7
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 17:13:52 +0100

    add form and fieldset into PresentQuestion.vue

--- src/components/PresentQuestion.vue
+++ src/components/PresentQuestion.vue
@@ -1,12 +1,33 @@
-<template>
-  <div>
-    <router-view></router-view>
-  </div>
-</template>
-
 <script setup>
 
+import { useStore } from 'vuex';
+
 </script>
+
+<template>
+<main class="container mx-auto" px-4>
+    <!--Question 1-->
+    <h1 class="mb-3 text-3xl">QUESTION</h1>
+    <form>
+
+<!--Checkbox area-->
+      <fieldset class="mb-3">
+      <div class = "">
+        <input type="checkbox" id = "checkbox"/>
+      </div>  
+      </fieldset>
+
+<!--Submit the answer-->
+      <fieldset class="mb-3">
+        <button class = "submitAnswerBtn" type = "submit">Submit answer</button>
+      </fieldset>
+    </form>
+    
+    </main>
+    <router-view></router-view>
+</template>
+
+
 
 <style scoped>
 
