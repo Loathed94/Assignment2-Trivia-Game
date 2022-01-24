6919b9b473ddbe483612e8295e6d37e33d77b44b
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-24 15:52:02 +0100

    feat: Added correct route for Selection and Start aswell as adding authGuard and loginGuard.

--- src/router.js
+++ src/router.js
@@ -1,12 +1,35 @@
 import {createRouter, createWebHistory} from "vue-router";
 import store from "./store";
 import Selection from "./views/Selection.vue"
+import Start from "./views/Start.vue"
+
+const loginGuard = (_to, _from, next) => {
+    if(store.state.user){
+        next("/selection")
+    }
+    else{
+        next()
+    }
+}
+const authGuard = (to, from, next) => {
+    if(!store.state.user){
+        next("/")
+    }
+    else{
+        next()
+    }
+}
 
 const routes = [
     {
         path: "/",
+        component: Start,
+        beforeEnter: loginGuard
+    },
+    {
+        path: "/selection",
         component: Selection,
-        //beforeEnter: "nån authGuard-aktig-grej"
+        beforeEnter: authGuard
     },
 ];
 
