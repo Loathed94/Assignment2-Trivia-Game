e4582ba3e8895a9f67a963906f5819bdcedd34b0
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 16:33:53 +0100

    add and remove code

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -2,33 +2,18 @@
     <!--Question 1-->
 
     <!--Create checkbox for answers of questions-->
-    <form @submit.prevent = "handleSubmit">
+    <form>
+      <fieldset>
       <div class = "">
         <label for = "title">Question</label>
         <input type="checkbox" id = "checkbox"/>
       </div>  
-
+      </fieldset>
   <!--Submit the answer-->
-      <div class = "submitAnswer">
-      </div>
-      <div class = "submitAnswer">
         <button class = "submitAnswerBtn" type = "submit">Submit answer</button>
-      </div>
     </form>
 </template>
-
-<!-- <script setup>
-//Show answer choices
-  export default {
-    data() {
-      Answers: [
-        {}
-        {}
-        {}
-        {} -->
-      ]
-    }
-  }
+<script setup>
 </script>
 
 <style scoped>
