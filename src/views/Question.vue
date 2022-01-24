280f58bf2b22adeeeae1f100b20782dd836858a8
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 17:14:32 +0100

    remove template context and add it to PresentQuestion.vue

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -10,24 +10,6 @@
 </script>
 
 <template>
-<main class="container mx-auto" px-4>
-    <!--Question 1-->
-    <h1 class="mb-3 text-3xl">QUESTION</h1>
-    <form>
-
-<!--Checkbox area-->
-      <fieldset class="mb-3">
-      <div class = "">
-        <input type="checkbox" id = "checkbox"/>
-      </div>  
-      </fieldset>
-
-<!--Submit the answer-->
-      <fieldset class="mb-3">
-        <button class = "submitAnswerBtn" type = "submit">Submit answer</button>
-      </fieldset>
-    </form>
-    </main>
 </template>
 
 <style scoped>
