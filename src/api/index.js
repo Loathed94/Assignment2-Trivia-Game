2f45076cf572c50116cf0c1bcf83b73f5b6b6bf8
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 22:05:32 +0100

    add url for questions in index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,3 @@
 export const BASE_URL = "https://lechan-noroff-api.herokuapp.com/";
 export const API_KEY = "n1ZKh0vT2oxeCvrWBefmxhBLDpFw0bouiHRtG1OjzIgol1Lm2TWdWSIcD4kj8Zic";
+export const BASE_URL_Question_DB = "https://opentdb.com/";
