f5b7c435324c6cb08c38972b55b8ee8ce2def87f
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 17:11:53 +0100

    add useRouter and useStore in Question.vue

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -3,7 +3,10 @@
   import { useStore } from 'vuex';
   import { useRouter } from 'vue-router';
   import PresentQuestion from '../components/PresentQuestion.vue'
-  
+
+  const router = useRouter();
+  const store = useStore();
+
 </script>
 
 <template>
