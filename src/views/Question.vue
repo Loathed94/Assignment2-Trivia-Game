8561f581d5622486367e4aa1fef01dbfb7d0cf47
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 17:10:12 +0100

    add imports on Question.vue

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,20 +1,31 @@
-<template>
-    <!--Question 1-->
+<script setup>
+  import { onMounted } from 'vue';
+  import { useStore } from 'vuex';
+  import { useRouter } from 'vue-router';
+  import PresentQuestion from '../components/PresentQuestion.vue'
+  
+</script>
 
-    <!--Create checkbox for answers of questions-->
+<template>
+<main class="container mx-auto" px-4>
+    <!--Question 1-->
+    <h1 class="mb-3 text-3xl">QUESTION</h1>
     <form>
-      <fieldset>
+
+<!--Checkbox area-->
+      <fieldset class="mb-3">
       <div class = "">
-        <label for = "title">Question</label>
         <input type="checkbox" id = "checkbox"/>
       </div>  
       </fieldset>
-  <!--Submit the answer-->
+
+<!--Submit the answer-->
+      <fieldset class="mb-3">
         <button class = "submitAnswerBtn" type = "submit">Submit answer</button>
+      </fieldset>
     </form>
+    </main>
 </template>
-<script setup>
-</script>
 
 <style scoped>
 
