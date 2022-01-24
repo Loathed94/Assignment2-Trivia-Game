97383a2d1fbe5c435479fcdb7d0004b9ec94f003
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 22:16:37 +0100

    add button, label and input about questions in Question.vue

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -5,6 +5,14 @@
 
 <template>
 <h1>Press down below to get a question!</h1>
+
+<form>
+    <field>
+        <button>Get question</button>
+        <label>Question name</label>
+        <input type="checkbox" id="question.name"/>
+    </field>
+</form>
 </template>
 
 <style scoped>
