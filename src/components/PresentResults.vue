58fb749bca4dd0f7c5b58dfe265cf6b8379db643
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-24 20:06:55 +0100

    feat: Added ul in view with li in component and made sure to allow communication between them.
    And some other minor details pertaining to the presentation of results in the view. No results or method for producing results exists yet.

--- src/components/PresentResults.vue
+++ src/components/PresentResults.vue
@@ -1,10 +1,20 @@
-<template>
-  
-</template>
-
 <script setup>
-
+    const props = defineProps({
+        result: {
+            type: Object,
+            required: true
+        }
+    })
 </script>
+
+<template>
+    <li>
+        <div>{{result.question}}</div>
+        <div>{{result.correct_answer}}</div>
+        <div>{{result.user_answer}}</div>
+        <button class="bg-yellow-500 p-3 rounded text-sm" @click="onFavouriteClick">Favourite</button>
+    </li>
+</template>
 
 <style scoped>
 
--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,10 +1,18 @@
-<template>
-  
-</template>
-
 <script setup>
+import { useStore } from 'vuex';
+import {computed} from "vue";
+import PresentResults from '../components/PresentResults.vue';
 
+const store = useStore();
+
+const results = computed(() => store.state.results)
 </script>
+
+<template>
+  <ul>
+      <PresentResults v-for="result in results" :key="result.question" :result="result" />
+  </ul>
+</template>
 
 <style scoped>
 
