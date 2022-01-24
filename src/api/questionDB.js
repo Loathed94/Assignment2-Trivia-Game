00d14bc283af20002e2b57700f1fa02c2d34b81f
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-24 18:05:54 +0100

    add category to async function in questionDB.js

--- src/api/questionDB.js
+++ src/api/questionDB.js
@@ -1,8 +1,8 @@
 import { BASE_URL_Question_DB } from ".";
 
-export async function apiFetchQuestions(category, quantityVal){
+export async function apiFetchQuestions(category){
     try {
-        const response = await fetch(`${BASE_URL_Question_DB}/api.php?amount=${quantityVal}&category=${category}`);
+        const response = await fetch(`${BASE_URL_Question_DB}/api.php?amount=&category=${category}`);
         const data = await response.json();
         return [data.response_code, data.results];
     } catch (error) {
