f3b32cb321a8b8b88bdbb780a1ddfa99fc8fa4d9
TheNeonLeon <leon.listo@hotmail.com>
2022-01-25 14:16:09 +0100

    FEAT: Added error message on register failure
    Error message shows up when trying to register with username already taken now

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -6,7 +6,6 @@ import {useStore} from "vuex"
 const store = useStore()
 const emit = defineEmits(["onAuthSuccess"]);
 const username = ref("");
-const password = ref("");
 const displayError = ref("");
 
 const onSuccess = user =>{
@@ -16,6 +15,7 @@ const onSuccess = user =>{
 }
 const onFailure = msg => {
   console.log(msg) //Bör va ett errormeddelande på vyn.
+  displayError.value = msg
 }
 
 const onRegister = async () => {
@@ -60,19 +60,10 @@ const onLogin = async () => {
       />
     </fieldset>
 
-    <fieldset>
-      <label for="password">Password</label>
-      <input
-        class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="password"
-        placeholder="******"
-        v-model="password"
-      />
-    </fieldset>
     <button @click="onRegister" type="button">Register</button>
     <button @click="onLogin" type="button">Login</button>
   </form>
-  <div v-if="onFailure">
+  <div v-if="displayError" class="bg-red-100 border border-red-400 text-red-700 px-4 py-2">
     <p>
       {{displayError}}
     </p>
