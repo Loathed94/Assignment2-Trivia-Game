ea1d954cf8d5be9d398e3fa45f03c9b675523c96
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-25 10:02:23 +0100

    change parameter to match radio button div name

--- src/api/game.js
+++ src/api/game.js
@@ -1,6 +1,6 @@
 import {BASE_URL_Question_DB} from './';
 
-export async function apiAnswerRegister(answerCheckbox) {
+export async function apiAnswerRegister(questionAnswerOptions) {
     try {
         const response = await fetch(`${BASE_URL_Question_DB}/api.php?amount=${questionQuantity}&category=${questionCategory}&difficulty=${questionDifficulty}&type=${questionType}`);
         const data = await response.json();
