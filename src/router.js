bb1630f99f30cb1cbd590fae8c3a58b178d8e679
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-25 01:37:27 +0100

    add result.vue to router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Question from './views/Question.vue';
+import Result from './views/Result.vue';
 
 const routes = [
     {
         path: "/",
         component: Question
+    },
+    {
+        path: "/result",
+        component: Result
     }
 ];
 
