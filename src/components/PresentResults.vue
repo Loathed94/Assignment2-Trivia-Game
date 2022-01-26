af53a7d5281fbf80aa93cb2b9feedbf209f72057
Leon Listo <Leon.listo@hotmail.com>
2022-01-26 09:25:54 +0100

    FIX: code gets rendered on page now in result vue

--- src/components/PresentResults.vue
+++ src/components/PresentResults.vue
@@ -1,18 +1,28 @@
 <script setup>
+import {useStore} from "vuex"
+import {computed, ref} from 'vue'
+const store = useStore()
 
     const props = defineProps({
-        result: {
+        results: {
             type: Object,
             required: true
         }
     })
+    const results = computed(() => store.state.results);
+
+    const test = [ results.incorrect_answers, results.incorrect_answers, results.incorrect_answers];
+const firstAnswer = ref(test[0]);
+
+
 </script>
 
 <template>
     <li class="mb-10">
-        <div>Question: {{result.question}}</div>
-        <div>Correct answer: {{result.correct_answer}}</div>
-        <div>User's answer: {{result.user_answer}}</div>
+        <h1>{{results}}</h1>
+        <div >Question: {{results.question}}</div>
+       <div> Correct answer: {{results.correct_answer}}</div>
+        <div>User's answer: {{results.user_answer}}</div> 
     </li>
 </template>
 
--- src/views/Result.vue
+++ src/views/Result.vue
@@ -2,8 +2,15 @@
 import { useStore } from 'vuex';
 import {computed, ref} from "vue";
 import PresentResults from '../components/PresentResults.vue';
-
 const store = useStore();
+
+    const props = defineProps({
+        results: {
+            type: Object,
+            required: true
+        }
+    })
+
 const calculateScore = () => {
   for (const question of results.value) {
     if(question.correct_answer === question.user_answer){
@@ -21,6 +28,7 @@ console.log(score.value);
 <template>
   <ul>
       <PresentResults v-for="result in results" :key="result.question" :result="result" />
+
   </ul>
   <div>Current highscore: </div>
   <div>Total score from game session: {{score}}</div>
